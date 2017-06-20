47406a6406ecff327ae3894aa4c1f73fb1f1e506
zhuozengsi <1534725079@qq.com>
2017-06-20 22:59:58 +0800

    init a c++ project environment in Xcode

---
+++ c_plus_plus_exercise/c_plus_plus_exercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_plus_plus_exercise
+//
+//  Created by Zhuozengsi on 20/06/2017.
+//  Copyright © 2017 Zhuozengsi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
